d0d2d3bb566f592d5a22ca3fc760ae6d62cc0bb8
Patric Campbell <patric@patriccampbell.com>
2017-10-14 23:52:14 -0400

    Add process args

--- github-grabber/animal_fun.js
+++ github-grabber/animal_fun.js
@@ -9,11 +9,25 @@ const fs = require("fs");
 //   }
 // });
 
-fs.writeFile("./example.txt", "I will be written to example.txt", err => {
-  if (err) {
-    console.log(err);
-    return;
-  } else {
-    console.log("file successfully created");
-  }
-});
+// fs.writeFile("./example.txt", "I will be written to example.txt", err => {
+//   if (err) {
+//     console.log(err);
+//     return;
+//   } else {
+//     console.log("file successfully created");
+//   }
+// });
+// let letter = process.args[2];
+
+// fs.readFile(".amimals.txt", "utf-8", (err, data) => {
+//   if (err) {
+//     console.log(err);
+//     return;
+//   } else {
+//     writeAnimals(letter);
+//   }
+// });
+
+// const writeAnimals = letter => {
+//   fs.writeFile(`${letter}_animals.txt`);
+// };
